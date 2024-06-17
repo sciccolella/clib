88f4ffc33e70ab68de38daf88826cdaf3068533f
Simone Ciccolella <simone.ciccolella@unimib.it>
2024-06-17 17:10:05 +0200

    inline tracing

--- benchmark/tracing.h
+++ benchmark/tracing.h
@@ -1,3 +1,4 @@
+// vim:ft=c
 #ifndef TRACING_H
 #define TRACING_H
 
@@ -9,9 +10,33 @@
 /*
  * Add `y` to `x`
  */
-void timeval_add(struct timeval *x, struct timeval *y);
+static inline void timeval_add(struct timeval *x, struct timeval *y) {
+  x->tv_sec += y->tv_sec;
+  x->tv_usec += y->tv_usec;
+  if (x->tv_usec >= 1000000) {
+    x->tv_sec++;
+    x->tv_usec -= 1000000;
+  } else if (x->tv_usec <= -1000000) {
+    x->tv_sec--;
+    x->tv_usec += 1000000;
+  }
+}
 
-struct timeval timeval_subtract(struct timeval *x, struct timeval *y);
+static inline struct timeval timeval_subtract(struct timeval *x, struct timeval *y) {
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_usec < y->tv_usec) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    y->tv_usec -= 1000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_usec - y->tv_usec > 1000000) {
+    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+    y->tv_usec += 1000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+  return (struct timeval){.tv_sec = x->tv_sec - y->tv_sec,
+                          .tv_usec = x->tv_usec - y->tv_usec};
+}
 
 typedef struct {
   clock_t c;
@@ -21,20 +46,43 @@ typedef struct {
 /*
  * Create a new trace containing current time
  */
-trace_t trace_start();
+static inline trace_t trace_start() {
+  clock_t c = clock();
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (trace_t){
+      .c = c,
+      .t = t,
+  };
+}
 /*
  * Increment `dest` trace with the values computed from `now - start`
  */
-void trace_incnow(trace_t *dest, trace_t *start);
+static inline void trace_incnow(trace_t *dest, trace_t *start) {
+  trace_t now = trace_start();
+  struct timeval tdiff = timeval_subtract(&now.t, &start->t);
+  timeval_add(&dest->t, &tdiff);
+  dest->c += now.c - start->c;
+}
 /*
  * Print the tracing of `t - now` with `name` as recognizer
  */
-void printtrace_diffnow(trace_t *t, const char *name);
+static inline void printtrace_diffnow(trace_t *t, const char *name) {
+  struct timeval t1, d;
+  clock_t c1 = clock();
+  gettimeofday(&t1, NULL);
+  d = timeval_subtract(&t1, &t->t);
+  fprintf(stderr, "[%s] time: %8ld:%04ld s:us\t CPU: %8ld s\n", name, d.tv_sec,
+          d.tv_usec, (c1 - t->c) / CLOCKS_PER_SEC);
+}
 /*
  * Print the tracing `t`, meant to be used with incremental tracing
  * `trace_inc*()` functions
  */
-void printtrace(trace_t *t, const char *name);
+static inline void printtrace(trace_t *t, const char *name) {
+  fprintf(stderr, "[%s] time: %8ld:%04ld s:us\t CPU: %8ld s\n", name,
+          t->t.tv_sec, t->t.tv_usec, t->c / CLOCKS_PER_SEC);
+}
 /*
  * Trace computation time of function `f`, if the function has
  * a return value, it should be passed in the variadic argument.
