7ca46a8dc02ee0e4ad67364f2ac551dd113fce2a
Simone Ciccolella <ciccolella.simone@gmail.com>
2024-06-24 15:21:30 +0200

    update benchmark with pointer

--- benchmark/b-hash.c
+++ benchmark/b-hash.c
@@ -3,11 +3,21 @@
 #include "tracing.h"
 #include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+#define print_int_debug(hx)                                                    \
+  for (size_t i = 0; i < (hx)->c; i++) {                                       \
+    printf("%3zu: %10d (h:%2zu) %20u", i, *(uint32_t *)chm_kat(hx, i),         \
+           (hx->khash)(chm_kat(hx, i))&hx->mod, *(uint32_t *)chm_vat(hx, i));          \
+    printf("\t%p", (uint32_t *)chm_vat(hx, i));                                \
+    puts("");                                                                  \
+  };
 
 #define vlit(v)                                                                \
   &(__typeof__((v))) { (v) }
 
+// #define __AVX2__
 KHASH_MAP_INIT_INT(32, uint32_t)
 int main() {
 #ifdef __AVX2__
@@ -24,7 +34,7 @@ int main() {
 
   trace_t s;
   size_t MAX = 5000000;
-  // MAX = 50000;
+  // MAX = 10000;
   int SEED = 21;
 
   // -----------------------------------------
@@ -33,10 +43,10 @@ int main() {
   srand(SEED);
   s = trace_start();
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
+    key = rand();
     k = kh_put(32, h, key, &ret);
     if (k != kh_end(h)) {
-      kh_value(h, k) = value;
+      kh_value(h, k) = key;
     }
   }
   printtrace_diffnow(&s, "khash  I");
@@ -45,9 +55,9 @@ int main() {
   srand(SEED);
   s = trace_start();
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
+    key = rand();
     k = kh_get(32, h, key);
-    // assert(kh_value(h, k) == value);
+    assert(kh_value(h, k) == key);
   }
   printtrace_diffnow(&s, "khash  G");
 
@@ -58,8 +68,8 @@ int main() {
   s = trace_start();
   chash *ch = chash_init(sizeof(uint32_t), sizeof(uint32_t));
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
-    chash_il(ch, vlit(key), vlit(value));
+    key = rand();
+    chash_il(ch, vlit(key), vlit(key));
   }
   printtrace_diffnow(&s, "clib-l I");
 
@@ -67,9 +77,9 @@ int main() {
   srand(SEED);
   s = trace_start();
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
+    key = rand();
     get = (uint32_t *)chash_gl(ch, vlit(key));
-    // assert(*get == value);
+    assert(*get == key);
   }
   printtrace_diffnow(&s, "clib-l G");
 
@@ -80,8 +90,8 @@ int main() {
   s = trace_start();
   chash *ch2 = chash_init(sizeof(uint32_t), sizeof(uint32_t));
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
-    chash_i_avx2_u4(ch2, vlit(key), vlit(value));
+    key = rand();
+    chash_i_avx2_u4(ch2, vlit(key), vlit(key));
   }
   printtrace_diffnow(&s, "clib-s I");
 
@@ -89,9 +99,9 @@ int main() {
   srand(SEED);
   s = trace_start();
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
+    key = rand();
     get = (uint32_t *)chash_g_avx2_u4(ch2, vlit(key));
-    // assert(*get == value);
+    assert(*get == key);
   }
   printtrace_diffnow(&s, "clib-s G");
 #endif
@@ -125,28 +135,28 @@ int main() {
   s = trace_start();
   chash *chAUTO = chash_init(sizeof(uint32_t), sizeof(uint32_t));
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
-    chash_i(chAUTO, vlit(key), vlit(value));
+    key = rand();
+    chash_i(chAUTO, vlit(key), vlit(key));
   }
-  printtrace_diffnow(&s, "clib-A I");
+  printtrace_diffnow(&s, "clib-x I");
 
   // get
   srand(SEED);
   s = trace_start();
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
+    key = rand();
     get = (uint32_t *)chash_g(chAUTO, vlit(key));
-    // assert(*get == value);
+    assert(*get == key);
   }
-  printtrace_diffnow(&s, "clib-A G");
+  printtrace_diffnow(&s, "clib-x G");
   // -----------------------------------------
   //  CHASH AUTO 2
   srand(SEED);
   s = trace_start();
   chash *chAUTO2 = chash_init(sizeof(uint32_t), sizeof(uint32_t));
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
-    chash_iA(chAUTO2, vlit(key), vlit(value));
+    key = rand();
+    chash_iA(chAUTO2, vlit(key), vlit(key));
   }
   printtrace_diffnow(&s, "clib-A I");
 
@@ -154,10 +164,30 @@ int main() {
   srand(SEED);
   s = trace_start();
   for (size_t i = 0; i < MAX; i++) {
-    key = rand(), value = rand();
+    key = rand();
     get = (uint32_t *)chash_gA(chAUTO2, vlit(key));
-    // assert(*get == value);
+    assert(*get == key);
   }
   printtrace_diffnow(&s, "clib-A G");
+  // -----------------------------------------
+  //  CHASH POINTER
+  srand(SEED);
+  s = trace_start();
+  chash *chp = chash_init(sizeof(uint32_t), sizeof(uint32_t));
+  for (size_t i = 0; i < MAX; i++) {
+    key = rand();
+    chash_pi(chp, vlit(key), vlit(key));
+  }
+  printtrace_diffnow(&s, "clib-P I");
+
+  // get
+  srand(SEED);
+  s = trace_start();
+  for (size_t i = 0; i < MAX; i++) {
+    key = rand();
+    get = (uint32_t *)chash_pg(chp, vlit(key));
+    assert(*get == key);
+  }
+  printtrace_diffnow(&s, "clib-P G");
   return EXIT_SUCCESS;
 }
